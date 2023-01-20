7791b58986f6699c5f1d19e3f274fe1dcce07305
vv6uos <wanamzz600@gmail.com>
2023-01-20 16:44:32 +0900

    test 연결 1

--- lib/test1.dart
+++ lib/test1.dart
@@ -1,3 +1,24 @@
 String test() {
+  List<String> worksheets = worksheet.split('\n');
+
+  //rev (커밋번호) 를 분리해서 따로 저장하기 위한 커밋 횟수
+
+  int commitNum = worksheet.split('rev').length - 1;
+  int commitIndex = 0;
+  int fileIndex = 0;
+
+  String title;
+  String result;
+
+  // commit No을 저장하기 위한 리스트
+  List<int> commitArr;
+  //파일 path를 저장하기 위한 리스트
+  List<String> file_pathArr;
+
+  int i = 0;
+  while (i < worksheets.length) {
+    print(worksheets[i]);
+  }
+
   return "하이";
 }
